c10d7b93c72a7eaf69365974ab97398040e2cb8a
mdotcoder <mmjunior0@gmail.com>
2023-01-14 23:43:53 -0500

    sidebar

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello from Codex',
+        message: 'Hello ChatGPT',
     })
 });
 
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
